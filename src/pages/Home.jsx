dd8dd42415e4456fdfad5b1f653eb6cc7b8ac90b
Sepideh Akbari <sepideh.akbarii@gmail.com>
2025-08-02 19:09:02 +0330

    update App.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import "../app.css"
+import "../App.css"
 import { useEffect } from "react"
 import { fetchProducts } from "../Redux/ProductSlice"
 import CartSlice from "../Redux/CartSlice"
